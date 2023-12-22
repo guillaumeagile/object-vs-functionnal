db58f3a5f1c97b1148951ab92af6fa86728fd348
guillaume iter <guillaume.saintetienne@iter.org>
2023-12-22 18:49:18 +0100

    Build avec sortie Optional, mais on veut mieux, on veut la liste des erreurs

--- src/main/java/org/example/BuilderEmployee.java
+++ src/main/java/org/example/BuilderEmployee.java
@@ -1,15 +1,19 @@
 package org.example;
 
-public record BuilderEmployee(String firstName , String lastName) {
+import java.util.Optional;
+
+public record BuilderEmployee(ProperName firstName , ProperName lastName) {
 
     public static BuilderEmployee WithFirstName(String firstName) {
-        return new BuilderEmployee(firstName, "");
+        return new BuilderEmployee(new ProperName( firstName), new ProperName( ""));
     }
     public BuilderEmployee WithLastName(String lastName) {
-        return new BuilderEmployee(this.firstName, lastName);
+        return new BuilderEmployee( this.firstName, new ProperName( lastName) );
     }
 
-    public Employee Build() {
-        return new Employee(this.firstName, this.lastName, 18);
+    public Optional<Employee> Build() {
+        if (this.firstName().validate() && this.lastName.validate())
+            return Optional.of( new Employee(this.firstName.name(), this.lastName.name(), 18));
+        return Optional.empty();
     }
 }
--- src/test/java/org/example/Validate4_BuildersFluentVaildation.java
+++ src/test/java/org/example/Validate4_BuildersFluentVaildation.java
@@ -8,15 +8,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Validate4_BuildersFluentVaildation {
 
     @Test
-    void RuleValidEmployee() {
+    void BuildValidEmployee() {
         var result =
               BuilderEmployee.WithFirstName("Gaston").WithLastName("Gui").Build();
 
         // OBJECTIF : utiliser un truc comme     RuleFor(customer => customer.Surname).NotNull().NotEqual("foo");
         // inspiré de https://docs.fluentvalidation.net/en/latest/start.html
 
-        assertThat( result.firstNameIsValid()).isTrue();
+        assertThat( result.isPresent()).isTrue();
       //  assertThat(result).contains(new ErrorEmployee("Invalid Lastname", 1));
+    }
+
+    @Test
+    void BuildInValidEmployee() {
+        var result =
+                BuilderEmployee.WithFirstName("").WithLastName("Gui").Build();
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void BuildInValidEmployeeEmpttyLastName() {
+        var result =
+                BuilderEmployee.WithFirstName("DD").WithLastName("").Build();
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void BuildInValidEmployee2ErrorsAndAList() {
+        var result =
+                BuilderEmployee.WithFirstName("").WithLastName("").Build();
+        assertThat(result.isFail()).isTrue();
+     //   assertThat(result.MatchLeft( err => assertThat(err).Count() == 2));
     }
 
 }
