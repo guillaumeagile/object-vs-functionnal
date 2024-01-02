e23eeee9c497608f9b3b59cd752278fc4b0d2362
guillaume iter <guillaume.saintetienne@iter.org>
2024-01-02 18:48:14 +0100

    next TODO

--- src/main/java/org/example/vavr/EmployeeValidator.java
+++ src/main/java/org/example/vavr/EmployeeValidator.java
@@ -13,16 +13,19 @@ public class EmployeeValidator {
 
     private static final int MIN_AGE = 18;
 
+    // TODO: utilser ErrorEmploye au lieu de String dans Seq
     public Validation<Seq<String>, Employee> validatePerson(String firstname, String lastName, int age) {
          return Validation.combine(validateName(firstname), validateName(lastName), validateAge(age)).ap(Employee::new);
     }
 
+    // TODO: utilser ErrorEmploye au lieu de String dans la partie de gauche (left)
     public Validation<String, String> validateName(String name) {
 
         return
                 Validation.invalid(STR."Nboo: \{name}");
     }
 
+    // TODO: utilser ErrorEmploye au lieu de String dans la partie de gauche (left)
     public  Validation<String, Integer> validateAge(int age) {
         if (age >= 18)
             return  Validation.valid(age);
